7da89be705b3ea50cd3b8dbd9baa662d81d06ecb
Logan Hasson <logan.hasson@gmail.com>
2016-08-25 00:18:20 -0400

    Looks better in this order

--- collatz.exs
+++ collatz.exs
@@ -2,6 +2,6 @@ defmodule Collatz do
   def collatz(n), do: collatz(n, 0)
 
   defp collatz(1, count), do: count
-  defp collatz(n, count) when rem(n, 2) == 0, do: collatz(div(n, 2), count + 1)
   defp collatz(n, count), do: collatz(div(3*n + 1,2), count + 2)
+  defp collatz(n, count) when rem(n, 2) == 0, do: collatz(div(n, 2), count + 1)
 end
